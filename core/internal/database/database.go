d2ea9abe331eb92be60859fe2ecb45c7384e3d77
Brett Evrist <brettevrist10@gmail.com>
2022-05-01 21:37:23 -0700

    progress

--- core/internal/database/database.go
+++ core/internal/database/database.go
@@ -1,1 +1,64 @@
 package database
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"strconv"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	_ "github.com/bevrist/simple-notify/core/pkg/api"
+)
+
+var db *sql.DB
+
+//init opens database and initializes if necessary
+func init() {
+	log.Println("database.init()")
+	//Create a folder/directory at a full qualified path
+	err := os.MkdirAll("./data/", 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, _ = sql.Open("sqlite3", "./data/database.db")
+
+	// create main table
+	statement, _ := db.Prepare("CREATE TABLE IF NOT EXISTS db (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+	statement.Exec()
+	statement.Close()
+
+	// create metadata table
+	statement, _ = db.Prepare("CREATE TABLE IF NOT EXISTS meta (id INTEGER PRIMARY KEY, version TEXT)")
+	statement.Exec()
+	statement.Close()
+
+	// cache sql statements
+	stInsert, _ = db.Prepare("INSERT INTO db (firstname, lastname) VALUES (?, ?)")
+}
+
+var stInsert *sql.Stmt
+
+func AddPeep(fn, ln string) {
+	stInsert.Exec(fn, ln)
+}
+
+// func GetMessages() []api.Message {
+func GetMessages() []string {
+	// var row struct {
+	//   age  int
+	//   name string
+	// }
+	// err = db.QueryRow("SELECT|people|age,name|age=?", 3).Scan(&row.age, &row.name)
+	rows, _ := db.Query("SELECT id, firstname, lastname FROM db")
+	var id int
+	var firstname string
+	var lastname string
+	var list []string
+	for rows.Next() {
+		rows.Scan(&id, &firstname, &lastname)
+		list = append(list, strconv.Itoa(id)+": "+firstname+" "+lastname)
+	}
+	return list
+}
--- core/main.go
+++ core/main.go
@@ -1,34 +1,18 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/bevrist/simple-notify/core/internal/database"
 )
 
 func main() {
-	fmt.Println("start")
-	//Create a folder/directory at a full qualified path
-	err := os.MkdirAll("./data/", 0755)
-	if err != nil {
-		log.Fatal(err)
-	}
+	database.AddPeep("John", "Doe")
+	database.AddPeep("Jeff", "Bezos")
 
-	database, _ := sql.Open("sqlite3", "./data/bogo.db")
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
-	statement.Exec()
-	statement, _ = database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
-	statement.Exec("Rob", "Gronkowski")
-	rows, _ := database.Query("SELECT id, firstname, lastname FROM people VALUES")
-	var id int
-	var firstname string
-	var lastname string
-	for rows.Next() {
-		rows.Scan(&id, &firstname, &lastname)
-		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+	for _, message := range database.GetMessages() {
+		fmt.Println(message)
 	}
 }
